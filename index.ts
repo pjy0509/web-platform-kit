381e62bceaadadca972a2f88d6550eacd246d8e9
박준영 <pjy@humanwares.com>
2026-06-29 16:53:40 +0900

    update to 0.0.2

--- index.ts
+++ index.ts
@@ -274,7 +274,7 @@ function parseOS(): NameVersionPair<OS> {
         }
     }
 
-    if (name === 'ios' && compareVersion(version, '18.6') === 0) {
+    if (name === 'ios' && PlatformKit.compareVersion(version, '18.6') === 0) {
         const execs: RegExpExecArray | null = /\) Version\/([\d.]+)/.exec(currentUserAgent);
 
         if (execs !== null) {
@@ -446,28 +446,6 @@ function parseFromHighEntropyValues(): Promise<void> {
 }
 
 ready = parseFromHighEntropyValues();
-
-export function compareVersion(lhs: string, rhs: string): -1 | 0 | 1 {
-    const pa: string[] = lhs.split('.');
-    const pb: string[] = rhs.split('.');
-    const length: number = Math.max(pa.length, pb.length);
-
-    for (let i: number = 0; i < length; i++) {
-        let a: number;
-        let b: number;
-
-        if (i < pa.length) a = parseInt(pa[i], 10);
-        else a = 0;
-
-        if (i < pb.length) b = parseInt(pb[i], 10);
-        else b = 0;
-
-        if (a > b) return 1;
-        if (a < b) return -1;
-    }
-
-    return 0;
-}
 
 const PlatformKit: PlatformKitInstance = {
     version: packageJSON.version,
